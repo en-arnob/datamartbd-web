c0ff9d92cd3d5dde5d3fb1d8d18601103da0daa1
en-arnob <en.arnob@gmail.com>
2024-04-30 11:06:45 +0600

    collab section

--- src/components/HomeSections.jsx
+++ src/components/HomeSections.jsx
@@ -732,6 +732,38 @@ const HomeSections = () => {
           </div>
         </div>
       </section>
+
+      <section>
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+            <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+              <img
+                alt=""
+                src="https://images.unsplash.com/photo-1532619187608-e5375cab36aa?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="absolute inset-0 h-full w-full object-cover"
+              />
+            </div>
+
+            <div className="lg:py-24">
+              <h2 className="text-3xl font-bold sm:text-5xl">
+                Take Your Business to Success
+              </h2>
+
+              <p className="mt-4 text-gray-600">
+                Discover how our software can streamline your operations and
+                boost your productivity.
+              </p>
+
+              <a
+                href="#"
+                className="mt-8 inline-block rounded bg-gray-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+              >
+                Contact
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
